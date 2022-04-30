32d2c1a9b4bcad6051ef3eb2b5e55ad9ad8984e1
Sandeep Nunna <snunna@centrak.com>
2022-04-30 16:01:39 -0400

    Create a Book class and update all logic

---
+++ gradebook/src/GradeBook/Book.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {
+
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }        
+
+        public void AddGrade(double grade)
+        {   
+            grades.Add(grade);
+        }
+
+        public void ShowStats()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach(var number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"Averge Grade: {result:N2}");
+            Console.WriteLine($"Lowest Grade: {lowGrade:N2}");
+            Console.WriteLine($"Higesht Grade: {highGrade:N2}");
+        }
+
+        private List<double> grades;
+        private string name;
+    }
+}
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -7,25 +7,13 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            // Declare Variables
-            var grades = new List<double>() { 12.7, 10, 6.11, 4.1, 11.0};
-            grades.Add(56.1); 
-
-            // Total Grade
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-            foreach(var number in grades)
-            {
-                highGrade = Math.Max(number, highGrade);
-                lowGrade = Math.Min(number, lowGrade);
-                result += number;
-            }
-
-            result /= grades.Count;
-            Console.WriteLine($"Averge Grade: {result:N2}");
-            Console.WriteLine($"Lowest Grade: {lowGrade:N2}");
-            Console.WriteLine($"Higesht Grade: {highGrade:N2}");
+            
+            var book = new Book("Scott's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            
+            book.ShowStats();
         }
     }
 }
