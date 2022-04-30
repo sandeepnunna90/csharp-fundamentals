fc6e305d2e52c94de1cf1d4b228a989148326ec8
Sandeep Nunna <snunna@centrak.com>
2022-04-30 14:31:22 -0400

    Create gradebook console application

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare Variables
+            var grades = new List<double>() { 12.7, 10, 6.11, 4.1, 11.0};
+            grades.Add(56.1); 
+
+            // Total Grade
+            var result = 0.0;
+            foreach(var number in grades)
+            {
+                result += number;
+            }
+            Console.WriteLine($"Total Grade: {result:N2}");
+
+            // Average Grade
+            var average = result / grades.Count;
+            Console.WriteLine($"Averge Grade: {average:N3}");
+
+            // Hello Syntax
+            if(args.Length > 0) 
+            {
+                Console.WriteLine($"Hello {args[1]}!");
+            } 
+            else
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+    }
+}
